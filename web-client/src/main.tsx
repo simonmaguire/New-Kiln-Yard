8b193790796b7c1a83d1dc419d099784691ce963
Simon <simonmaguire@live.com>
2025-06-20 11:40:40 -0600

    load all pots

--- web-client/src/main.tsx
+++ web-client/src/main.tsx
@@ -7,6 +7,7 @@ import { AuthContextProvider } from './AuthContext.tsx'
 import { PotteryCollectionPage } from'./pages/PotteryCollection.tsx'
 import { ProtectedRoute } from './components/ProtectedRoute.tsx'
 import { AuthPage } from './pages/AuthPage.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const router = createBrowserRouter([
   { path: '/', element: <App />, children: [
@@ -17,10 +18,14 @@ const router = createBrowserRouter([
   ]}
 ])
 
+const queryClient = new QueryClient()
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AuthContextProvider>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </AuthContextProvider>
   </StrictMode>,
 )
--- web-client/src/pages/PotteryCollection.tsx
+++ web-client/src/pages/PotteryCollection.tsx
@@ -1,7 +1,43 @@
+import {useQuery} from '@tanstack/react-query'
+// import { useState } from 'react'
+
+
+interface IPot {
+    id: number,
+    clay : string,
+    stage : string,
+    category : string,
+    throw_date : Date,
+    clay_weight : number,
+    thrown_height : number,
+    thrown_width : number,
+    thrown_notes : string,
+    trim_date : Date,
+    trim_notes : string,
+    glazes : string[],
+    glaze_notes : string,
+    finished_width : number,
+    finished_height : number,
+    finished_notes : string,
+}
+
 export const PotteryCollectionPage = () => {
+    const {isPending, isError, error, data, isLoading} = useQuery({queryKey: ['pots'], queryFn: async () => {
+        const response = await fetch(`${import.meta.env.VITE_SERVER_URL}/pots`)
+        if (!response.ok) {
+          throw new Error('Network response was not ok')
+        }
+        return response.json()
+      }})
+
+    if(isPending || isLoading) return (<span>Loading...</span>)
+
+    if(isError) return (<span>Error: {error.message}</span>)
+    
     return (
-        <div>
-            <h1>Logged In!</h1>
-        </div>
+        data.map((pot: IPot) => {
+               return <span key={pot.id }>{pot.id} {pot.category}</span>
+
+        })
     );
 }
